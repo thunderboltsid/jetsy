39e64d70134a764c5b7608a1b2451601d78f990b
Siddharth Shukla <siddharthshukla@outlook.com>
2014-12-25 06:36:13 +0100

    Add Users to app

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'autoprefixer-rails'
 gem 'paperclip', '~> 4.2'
 gem 'paperclip-dropbox', '>= 1.1.7'
 gem 'figaro'
+
+gem 'devise'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Required for devise, change host and port to actual application host
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
