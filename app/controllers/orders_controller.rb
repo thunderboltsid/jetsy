3057873c46eced4e15188b0f5ea2e499d350d8b8
Siddharth Shukla <siddharthshukla@outlook.com>
2014-12-26 03:10:34 +0100

    add order history page and modified navbar

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,24 +1,20 @@
 class OrdersController < ApplicationController
-  before_action :set_order, only: [:show, :edit, :update, :destroy]
+  before_action :set_order, only: [:edit, :update, :destroy]
   before_action :authenticate_user!
   respond_to :html
 
-  def index
-    @orders = Order.all
-    respond_with(@orders)
+  def sales
+    @orders = Order.all.where(seller: current_user).order('created_at DESC')
   end
 
-  def show
-    respond_with(@order)
+  def purchases
+    @orders = Order.all.where(buyer: current_user).order('created_at DESC')
   end
 
   def new
     @order = Order.new
     @listing = Listing.find(params[:listing_id])
     respond_with(@order)
-  end
-
-  def edit
   end
 
   def create
@@ -28,16 +24,6 @@ class OrdersController < ApplicationController
     @order.listing_id = @listing.id
     @order.buyer_id = current_user.id
     @order.save
-    respond_with(@order)
-  end
-
-  def update
-    @order.update(order_params)
-    respond_with(@order)
-  end
-
-  def destroy
-    @order.destroy
     respond_with(@order)
   end
 
