93785d25ca5b5825ab7a3b3c13a36dd0c416ab3d
Siddharth Shukla <siddharthshukla@outlook.com>
2015-01-15 21:32:31 +0100

    fix migration issues

--- db/schema.rb
+++ db/schema.rb
@@ -13,23 +13,23 @@
 
 ActiveRecord::Schema.define(version: 20141226010340) do
 
-  create_table "listings", force: true do |t|
-    t.string   "name"
+  create_table "listings", force: :cascade do |t|
+    t.string   "name",               limit: 255
     t.text     "description"
     t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
+    t.string   "image_file_name",    limit: 255
+    t.string   "image_content_type", limit: 255
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.integer  "user_id"
   end
 
-  create_table "orders", force: true do |t|
-    t.string   "address"
-    t.string   "city"
-    t.string   "state"
+  create_table "orders", force: :cascade do |t|
+    t.string   "address",    limit: 255
+    t.string   "city",       limit: 255
+    t.string   "state",      limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "listing_id"
@@ -37,20 +37,20 @@ ActiveRecord::Schema.define(version: 20141226010340) do
     t.integer  "seller_id"
   end
 
-  create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
+    t.string   "name",                   limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
